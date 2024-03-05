8aa65446cca37d74d937aa3dade87fb6b1f41e2b
Jory Phillips <jorylphillips@gmail.com>
2024-03-05 13:58:32 -0800

    Initial Commit

---
+++ sorted watch prototype Watch App/sorted_watch_prototypeApp.swift
@@ -0,0 +1,17 @@
+//
+//  sorted_watch_prototypeApp.swift
+//  sorted watch prototype Watch App
+//
+//  Created by Jory Phillips on 3/5/24.
+//
+
+import SwiftUI
+
+@main
+struct sorted_watch_prototype_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
